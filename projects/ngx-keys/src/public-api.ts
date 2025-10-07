b70ab357dd961d957546a85bd8dcae07d422c812
Miguel Rivas Perez <mrivasperez@icloud.com>
2025-10-06 18:20:21 -0700

    task: Update public-api

--- projects/ngx-keys/src/public-api.ts
+++ projects/ngx-keys/src/public-api.ts
@@ -5,9 +5,9 @@
 export * from './lib/core/keyboard-shortcuts.service';
 export * from './lib/models/keyboard-shortcut.interface';
 export * from './lib/errors/keyboard-shortcuts.errors';
-// Export configuration types and constants
 export { 
   DEFAULT_SEQUENCE_TIMEOUT_MS,
   KEYBOARD_SHORTCUTS_CONFIG
 } from './lib/config/keyboard-shortcuts.config';
 export type { KeyboardShortcutsConfig } from './lib/config/keyboard-shortcuts.config';
+export { KeyMatcher } from './lib/core/utils/key-matcher';
